440160a4b788e3a116c730cff63dc0919c4b3d0d
Naresh <nareshsuren@gmail.com>
2026-06-28 20:26:46 +0400

    fix: prevent Varnish caching API responses

--- api.php
+++ api.php
@@ -6,6 +6,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type, X-Api-Token');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Content-Type: application/json');
+header('Cache-Control: no-store, no-cache, must-revalidate');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
